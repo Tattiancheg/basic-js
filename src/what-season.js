d249d9d039c127bb5f8e6cfac2b7090e3563c5f4
Tattiancheg <levina.tatsiana@gmail.com>
2020-10-19 17:06:23 +0300

    fix:what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,24 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  throw new CustomError('Not implemented');
-  if (typeof(date) !== 'number') {
+  // throw new CustomError('Not implemented');
+  if (!date) return 'Unable to determine the time of year!';
+  if (typeof(date) !== 'object' || isNaN(date)) {
     throw new Error('');
   }
-  var regexp1 = /[3-5]/gi;
-  var matches1 = date.toString.match(regexp1);
 
-  var regexp2 = /[6-8]/gi;
-  var matches2 = date.toString.match(regexp2);
-
-  var regexp3 = /[9-11]/gi;
-  var matches3 = date.toString.match(regexp3);
-
-  var regexp4 = /[12,1-2]/gi;
-  var matches4 = date.toString.match(regexp4);
-
-  if (date[1] === matches1) return 'spring';
-  if (date[1] === matches2) return 'summer';
-  if (date[1] === matches3) return 'autumn';
-  if (date[1] === matches4) return 'winter';
-};
+var month = date.getMonth();
+  if ( month === 0 || month === 1 || month === 11) {
+      return 'winter';
+  } else if 
+    ( month === 2 || month === 3 || month === 4) {
+      return 'spring';
+  } else if 
+    ( month === 5 || month === 6 || month === 7) {
+      return 'summer';
+  } else if
+    ( month === 8 || month === 9 || month === 10) {
+      return 'autumn';
+  }
+  }
+  
