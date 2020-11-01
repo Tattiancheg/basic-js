c6eeb0975744dd92d3ec723856e9e0830bc67d6a
Tattiancheg <levina.tatsiana@gmail.com>
2020-11-01 17:29:33 +0300

    fix:carbon date with transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+  if (arr.length === 0) return arr;
+  
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == '--discard-next') {
+          if ((i + 1) < arr.length) i++;
+      } else if (arr[i] == '--discard-prev') {
+          if (i > 0 && arr[i-2] !== '--discard-next') res.pop();
+      } else if (arr[i] == '--double-next') {
+          if (i < arr.length -1) res.push(arr[i + 1]);
+      } else if (arr[i] == '--double-prev') {
+          if (i > 0 && arr[i-2] !== '--discard-next') res.push(arr[i - 1]);
+      } else {
+          res.push(arr[i]);
+      }
+
+  }
+  return res;
+}
+
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,13 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    encrypt() {
+        throw 'Not implemented';
+        // remove line with error and write your code here
+    }
+
+    decrypt() {
+        throw 'Not implemented';
+        // remove line with error and write your code here
+    }
 }
 
 module.exports = VigenereCipheringMachine;
